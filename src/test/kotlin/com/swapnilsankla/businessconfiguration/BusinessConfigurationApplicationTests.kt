e7c5b31e0248d2b573c43d2ffc6e4369c9c7f17d
Swapnil Sankla <swapnil.sankla@gmail.com>
2019-07-20 19:30:39 +0530

    Start mongod instance manually before integration test

--- src/test/kotlin/com/swapnilsankla/businessconfiguration/BusinessConfigurationApplicationTests.kt
+++ src/test/kotlin/com/swapnilsankla/businessconfiguration/BusinessConfigurationApplicationTests.kt
@@ -1,5 +1,13 @@
 package com.swapnilsankla.businessconfiguration
 
+import de.flapdoodle.embed.mongo.MongodExecutable
+import de.flapdoodle.embed.mongo.MongodStarter
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder
+import de.flapdoodle.embed.mongo.config.Net
+import de.flapdoodle.embed.mongo.distribution.Version
+import de.flapdoodle.embed.process.runtime.Network
+import org.junit.AfterClass
+import org.junit.BeforeClass
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
@@ -9,8 +17,31 @@ import org.springframework.test.context.junit4.SpringRunner
 @SpringBootTest
 class BusinessConfigurationApplicationTests {
 
+    companion object {
+        private lateinit var mongodExecutable: MongodExecutable
+
+        @BeforeClass
+        @JvmStatic
+        fun setUp() {
+            val mongodConfig = MongodConfigBuilder()
+                    .version(Version.Main.PRODUCTION)
+                    .net(Net("localhost", 27017, Network.localhostIsIPv6()))
+                    .build()
+
+            val starter = MongodStarter.getDefaultInstance()
+            mongodExecutable = starter.prepare(mongodConfig)
+            mongodExecutable.start()
+        }
+
+        @AfterClass
+        @JvmStatic
+        fun tearDown() {
+            mongodExecutable.stop()
+        }
+    }
+
+
     @Test
     fun contextLoads() {
     }
-
 }
